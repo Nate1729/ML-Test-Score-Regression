d5ca054093dd5f97b7abb14b1ae843d8ee71b4cb
LethalTedyBear <nferreira1729@gmail.com>
2021-06-25 14:08:51 -0400

    feat: :sparkles: Add error function
    Compute the error of a line of best fit.

--- src/main.py
+++ src/main.py
@@ -9,7 +9,13 @@ def read_data():
 			points.append([float(row[0]), float(row[1])])
 	return points
 
-	
+def compute_error(line, points):	
+	error = 0
+	for p in points:
+		x = p[0]
+		y = p[1]
+		error += (y - (line[0] + x*line[1])) ** 2
+		return error / len(points)
 
 
 
@@ -17,4 +23,4 @@ def read_data():
 
 
 if __name__ == '__main__':
-	print(read_data())
+	pass
