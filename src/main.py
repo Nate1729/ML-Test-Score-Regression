73d055ca2b1169f71107e2f5b6d24a52b16cf417
LethalTedyBear <nferreira1729@gmail.com>
2021-06-25 18:55:34 -0400

    feat: :sparkles: Add gradient step function
    Compute gradient step for a single epoc

--- src/main.py
+++ src/main.py
@@ -17,7 +17,19 @@ def compute_error(line, points):
 		error += (y - (line[0] + x*line[1])) ** 2
 		return error / len(points)
 
-
+def gradient_step(line_start, points, learning_rate):
+	b_gradient = 0
+	m_gradient = 0
+	N = float(len(points))
+	for p in points:
+		x = p[0]
+		y = p[1]
+		m_gradient += -(2/N) * (y - (line_start[1]*x + line_start[0]))*x
+		b_gradient += -(2/N) * (y - (line_start[1]*x + line_start[0]))
+	b_new = line_start[0] - (b_gradient * learning_rate)
+	m_new = line_start[1] - (m_gradient * learning_rate)
+	line_new = [b_new, m_new]
+	return line_new
 
 		
 
